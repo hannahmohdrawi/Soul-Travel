8805e98058064d814ce70c5e749d5477dbd25ec0
Hannah Mohd-Rawi <85315928+hannahmohdrawi@users.noreply.github.com>
2024-05-02 13:42:19 +0100

    javascript added to deals.html

--- deals/deals.js
+++ deals/deals.js
@@ -1,5 +1,21 @@
 
 
-function swipeUp(){
-    //Want to include when you click on that header then it transitions up and the travel package details appear 
+let hiddenMainText = function(){
+    document.querySelector('main-text') = style.visiblity = 'hidden';
 }
+
+let showMainText = function(){
+    document.querySelector('main-text') = style.visibility = 'visible';
+}
+
+
+let visibilityOptions = function(){
+    if (onclick = true){
+        return hiddenMainText;
+    }
+    else{
+        return showMainText
+    }
+}
+
+
